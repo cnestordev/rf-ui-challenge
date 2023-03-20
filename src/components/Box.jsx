a5b369f20a6072797761765c763f9cbc2c6bb38b
cnestordev <ncervan87@gmail.com>
2023-03-20 00:13:14 -0700

    adds step 1

---
+++ src/components/Box.jsx
@@ -0,0 +1,12 @@
+import '../styles/main.css'
+
+const Box = ({title, body}) => {
+    return (
+        <div className='step-card'>
+            <span>{title}</span>
+            <div>{body}</div>
+        </div>
+    )
+}
+
+export default Box;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import '../styles/main.css'
+import Box from './Box';
 
 const Main = () => {
     return (
@@ -6,6 +7,15 @@ const Main = () => {
             <div className="main-header">
                 <span className='icon'></span>
                 <h3>Attendees</h3>
+            </div>
+
+            <div className='step-container'>
+                <h3 className='step-title'><span>Step 1:</span> Base settings.</h3>
+                <div className='step-border'>
+                    <Box title="General" body="Define Attendee types & attributes" />
+                    <Box title="Title" body="Description that explains the value goes here. Description that explains the value goes here. " />
+                    <Box title="Title" body="Description that explains the value goes here. Description that explains the value goes here. " />
+                </div>
             </div>
         </div>
     )
